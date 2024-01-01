bcc3189b757d82d5dfb15a8caf90339347f49d5e
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-01-01 20:25:29 +0430

    complete the contact

--- biggner/src/app/contact/page.jsx
+++ biggner/src/app/contact/page.jsx
@@ -1,6 +1,25 @@
+
+import Image from "next/image"
+import styles from "./contact.module.css"
+
 const ContactPage = ()=>{
     return (
-        <div>Contact Page</div>
+        <div className={styles.container}>
+        <div className={styles.imgcontainer}>
+            <Image src="/contact.png" alt="" fill className={styles.img}/>
+        </div>
+        <div className={styles.formcontainer}>
+            <form action="" className={styles.form}>
+                <input type="text" placeholder="Name"/>
+                <input type="text" placeholder="Email"/>
+                <input type="text" placeholder="phone"/>
+                <textarea name="" cols="30" rows="10" placeholder="message" ></textarea>
+                <button className={styles.button}>Send</button>
+            </form>
+        </div>
+
+
+        </div>
     )
 }
 
