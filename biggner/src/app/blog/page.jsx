42ea22d2a8750d5cb5af3be757fd513b1ead1b58
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-01-09 18:45:25 +0430

    add new project

--- biggner/src/app/blog/page.jsx
+++ biggner/src/app/blog/page.jsx
@@ -1,25 +1,26 @@
-// FETCH DATA WITH AN API
-// const getData = async () => {
-// 	const res = await fetch("http://localhost:3000/api/blog", {
-// 		next: { revalidate: 3600 },
-// 	});
 import { getPosts } from "@/lib/data";
 import styles from "./blog.module.css";
 import PostCard from "@/component/postCard/postCard";
 
-// 	if (!res.ok) {
-// 		throw new Error("Something went wrong");
-// 	}
+// FETCH DATA WITH AN API
+const getData = async () => {
+	const res = await fetch("http://localhost:3000/api/blog", {
+		next: { revalidate: 3600 },
+	});
 
-// 	return res.json();
-// };
+	if (!res.ok) {
+		throw new Error("Something went wrong");
+	}
+
+	return res.json();
+};
 
 const BlogPage = async () => {
 	// FETCH DATA WITH AN API
-	// const posts = await getData();
+	const posts = await getData();
 
 	// FETCH DATA WITHOUT AN API
-	const posts = await getPosts();
+	// const posts = await getPosts();
 
 	return (
 		<div className={styles.container}>
